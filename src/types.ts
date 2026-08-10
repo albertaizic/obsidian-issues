bfa701b6b1783db6f457670a39aa6fa182123639
Albert Aizic <a.aizic@student.tudelft.nl>
2026-08-10 16:07:22 +0200

    feat: new issue modal

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,13 @@ import type { TFile } from 'obsidian';
 
 export type IssueStatus = 'open' | 'closed';
 
-export interface Issue {
-  id: string;
+export interface IssueData {
   title: string;
   status: IssueStatus;
+  created: string;
+}
+
+export interface Issue extends IssueData {
+  id: string;
   file: TFile;
 }
